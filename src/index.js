acd4a0d9c0b57d9f786206241d4fd5e5947e9a9b
James A. Petts <jamesapetts@gmail.com>
2022-01-17 10:00:52 +0000

    Update extension dependencies

--- src/index.js
+++ src/index.js
@@ -1,61 +1,70 @@
 import toolbarButtons from "./toolbarButtons.js";
 import { hotkeys } from "@ohif/core";
 
-const ohif = {
-  layout: "org.ohif.default.layoutTemplateModule.viewerLayout",
-  sopClassHandler: "org.ohif.default.sopClassHandlerModule.stack",
-  hangingProtocols: "org.ohif.default.hangingProtocolModule.default",
-  viewport: "org.ohif.cornerstone.viewportModule.cornerstone",
+const ohifDefault = {
+    layout: "org.ohif.default.layoutTemplateModule.viewerLayout",
+    sopClassHandler: "org.ohif.default.sopClassHandlerModule.stack",
+    hangingProtocols: "org.ohif.default.hangingProtocolModule.default"
+};
+
+const ohifCornerstone = {
+    viewport: "org.ohif.cornerstone.viewportModule.cornerstone"
 };
 
 const clock = {
-  panel: "extension.clock.panelModule.clockPanel",
+    panel: "extension.clock.panelModule.clockPanel"
 };
 
 export default function mode({ modeConfiguration }) {
-  return {
-    id: "clock",
-    displayName: "Basic Viewer",
-    /**
-     * Lifecycle hooks
-     */
-    onModeEnter: ({ servicesManager, extensionManager }) => {
-      const { ToolBarService } = servicesManager.services;
-      ToolBarService.init(extensionManager);
-      ToolBarService.addButtons(toolbarButtons);
-      ToolBarService.createButtonSection("primary", ["Time"]);
-    },
-    onModeExit: () => {},
-    validationTags: {
-      study: [],
-      series: [],
-    },
-    isValidMode: () => {},
-    routes: [
-      {
-        path: "clock",
-        layoutTemplate: ({ location, servicesManager }) => {
-          return {
-            id: ohif.layout,
-            props: {
-              leftPanels: [],
-              rightPanels: [clock.panel],
-              viewports: [
-                {
-                  namespace: ohif.viewport,
-                  displaySetsToDisplay: [ohif.sopClassHandler],
-                },
-              ],
-            },
-          };
+    return {
+        id: "clock",
+        displayName: "Basic Viewer",
+        /**
+         * Lifecycle hooks
+         */
+        onModeEnter: ({ servicesManager, extensionManager }) => {
+            const { ToolBarService } = servicesManager.services;
+            ToolBarService.init(extensionManager);
+            ToolBarService.addButtons(toolbarButtons);
+            ToolBarService.createButtonSection("primary", ["Time"]);
         },
-      },
-    ],
-    extensions: ["org.ohif.default", "org.ohif.cornerstone"],
-    hangingProtocols: [ohif.hangingProtocols],
-    sopClassHandlers: [ohif.sopClassHandler],
-    hotkeys: [...hotkeys.defaults.hotkeyBindings],
-  };
+        onModeExit: () => {},
+        validationTags: {
+            study: [],
+            series: []
+        },
+        isValidMode: () => {},
+        routes: [
+            {
+                path: "clock",
+                layoutTemplate: ({ location, servicesManager }) => {
+                    return {
+                        id: ohifDefault.layout,
+                        props: {
+                            leftPanels: [],
+                            rightPanels: [clock.panel],
+                            viewports: [
+                                {
+                                    namespace: ohifCornerstone.viewport,
+                                    displaySetsToDisplay: [
+                                        ohifDefault.sopClassHandler
+                                    ]
+                                }
+                            ]
+                        }
+                    };
+                }
+            }
+        ],
+        extensions: [
+            "org.ohif.default",
+            "org.ohif.cornerstone",
+            "extension.clock"
+        ],
+        hangingProtocols: [ohifDefault.hangingProtocols],
+        sopClassHandlers: [ohifDefault.sopClassHandler],
+        hotkeys: [...hotkeys.defaults.hotkeyBindings]
+    };
 }
 
 window.clockMode = mode({});
